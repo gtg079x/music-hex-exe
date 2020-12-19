a3776225feeceda646a2e9ba46f2e396da4895b8
gtg079x <67651413+gtg079x@users.noreply.github.com>
2020-12-19 16:18:22 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
-input.onButtonPressed(Button.A, function () {
-	
+gamerbit.onEvent(GamerBitPin.P12, GamerBitEvent.Down, function () {
+    t = 1
+    music.playMelody("C A F - G C - C5 ", 120)
 })
-input.onButtonPressed(Button.B, function () {
-	
+gamerbit.onEvent(GamerBitPin.P16, GamerBitEvent.Down, function () {
+    t = 2
+    music.playMelody("E B C5 A B G A F ", 120)
 })
-let t = 120
-basic.forever(function () {
-    music.playMelody("E B C5 A B G A F ", t)
-    t = t + 10
+let t = 0
+t = 120
+control.inBackground(function () {
+    while (true) {
+        if (t == 1) {
+            basic.showString("Song 1")
+        } else if (t == 2) {
+            basic.showString("Song 2")
+        } else {
+            basic.showString("No Song")
+        }
+    }
 })
